082e095a421e4ab8f853f4473143463d52bbae1b
Jekaterina Macilevic <jekaterina.macilevic@dailypay.com>
2024-12-16 17:11:22 +0000

    Now only authenticated user who created a specific event can update and delete it

--- Projects/LastProject/routes/events.go
+++ Projects/LastProject/routes/events.go
@@ -67,9 +67,16 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventByID(eventId)
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch an event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "You are not allowed to update this event."})
 		return
 	}
 
@@ -107,7 +114,14 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "You are not allowed to delete this event."})
+		return
+	}
+
 	err = event.Delete()
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete an event."})
 		return
